ccdbd53acbdca963cd600a4e7f461ffce3307b18
Thomas Erickson <terickson@spotxchange.com>
2017-01-21 19:05:31 -0700

    added test cases for GET todos by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo.js');
 
 const todos = [{
+  _id : new ObjectID(),
   text: 'first set of todos'
 },{
+  _id: new ObjectID(),
   text: 'second set of todos'
 }];
 
@@ -69,3 +72,31 @@ describe('GET /todos', () => {
       .end(done);
   });
 })
+
+describe('GET /todos/:id', () => {
+
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.text).toBe(todos[0].text)
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/stuff')
+      .expect(404)
+      .end(done);
+  });
+});
