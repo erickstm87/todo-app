b44687d03d3c920aead8764c2ba71f110e081ef7
Thomas Erickson <terickson@spotxchange.com>
2017-01-21 22:11:12 -0700

    setup heroku

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+const port = process.env.PORT || 3000;
 
 
 var app = express();
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
