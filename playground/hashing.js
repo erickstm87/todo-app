6b47935b61bc70c21833d8c92049227c00ff9201
Thomas Erickson <terickson@spotxchange.com>
2017-02-03 21:24:19 -0700

    added hashing to the passwords being sent in

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(`token is ${token}`);
-var decoded = jwt.verify(token, '123abc');
-console.log('docoded', decoded);
-// var message = 'tom\'s the bomb';
+// bcrypt.genSalt(10, (err,salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+var hashedPassword = '$2a$10$brC3dGVy/pfxWr4TLoFUzufnypRDcatXFswWB0ZyvcpJXTMMTe3/m';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(`token is ${token}`);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('docoded', decoded);
+// // var message = 'tom\'s the bomb';
 //
 // var hash = SHA256(message).toString();
 //
